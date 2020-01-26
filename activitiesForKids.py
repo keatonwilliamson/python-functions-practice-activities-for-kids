0224e4ab0d131f15b0163028ed992e261b581c00
George Williamson <keatonwilliamson@Georges-MacBook-Pro.local>
2020-01-26 13:06:08 -0600

    first commit

---
+++ activitiesForKids.py
@@ -0,0 +1,26 @@
+running_kids = ["Pam", "Sam", "Andrea", "Will"]
+swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+
+def run(kid_name):
+    print("%s is running" % (kid_name))
+for kid in running_kids:
+    run(kid)
+
+def swing(kid_name):
+    print("%s is swinging" % (kid_name))
+for kid in swinging_kids:
+    swing(kid)
+
+def slide(kid_name):
+    print("%s is sliding" % (kid_name))
+for kid in sliding_kids:
+    slide(kid)
+
+def hide(kid_name):
+    print("%s is hiding" % (kid_name))
+for kid in hiding_kids:
+    hide(kid)
+
+
